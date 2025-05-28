7ca1fe04f291e9b6f44a1e0d7c75f3ca8236987d
azez <79533966+azezsan@users.noreply.github.com>
2025-05-28 14:16:07 +0300

    MORE APPS

--- run_once_install-apps.sh
+++ run_once_install-apps.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-flatpak install flathub app.zen_browser.zen dev.vencord.Vesktop com.valvesoftware.Steam
+flatpak install flathub app.zen_browser.zen dev.vencord.Vesktop com.valvesoftware.Steam com.obsproject.Studio org.gimp.GIMP io.gitlab.theevilskeleton.Upscaler org.nickvision.tubeconverter io.missioncenter.MissionCenter com.github.huluti.Curtail it.mijorus.gearlever net.lutris.Lutris io.github.Foldex.AdwSteamGtk
